1833beb10f73544544e53b2ca72a3a665dfc6b99
Efim Pyshnograev <efim@fastmail.com>
2018-06-02 18:37:23 +1000

    Settings variable change

--- src/main/java/com/atlassian/graev/lock/snitch/agent/Settings.java
+++ src/main/java/com/atlassian/graev/lock/snitch/agent/Settings.java
@@ -3,7 +3,7 @@ package com.atlassian.graev.lock.snitch.agent;
 class Settings {
 
     private static final int MAX_TRACE_FILES = Integer.getInteger("lock.snitch.max.trace.files", 10);
-    private static final int RECURSION_DEPTH = Integer.getInteger("lock.snitch.guarding.recursion.depth", 100);
+    private static final int RECURSION_DEPTH = Integer.getInteger("lock.snitch.recursion.depth", 100);
 
     static int maxTraceFiles() {
         return MAX_TRACE_FILES;
